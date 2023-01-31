716f94bd6f8b63c13f6375b9b7cb003bc946cb83
DenisPepper <thech@yandex.ru>
2023-02-01 00:25:50 +0300

    created new TodoList FC

---
+++ src/components/todo-list/todo-list.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function TodoList(): JSX.Element {
+    return(
+        <>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </>
+    );
+}
